df55bea7fae884b081ff5e94a258cd6201663e68
kibet nathan <nathanrufus540@gmail.com>
2024-03-04 10:58:01 +0300

    solved error

--- server/app.py
+++ server/app.py
@@ -1,15 +1,8 @@
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
+
 from App import create_app
 
 app=create_app()
-# db = SQLAlchemy()
 
-
-# app = Flask(__name__)
-
-# app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///sensitize.db'
-# db.init_app(app)
 
 
 
