89ec4120e34139063238bcbb1d5a312c7e3263f5
kibet nathan <nathanrufus540@gmail.com>
2024-03-11 21:01:09 +0300

    sequilize function

--- src/components/db.js
+++ src/components/db.js
@@ -1,9 +1,11 @@
-const Sequelize = require("sequelize");
+import { Sequelize } from 'sequelize';
+
 const sequelize = new Sequelize(
-  "postgresql://sensitization_app_user:m43TYykCUSL9t1m9IBdSbYa5Og7n6n3g@dpg-cnin16v79t8c73bt7qr0-a.oregon-postgres.render.com/sensitization_app",
+  'postgresql://sensitization_app_user:m43TYykCUSL9t1m9IBdSbYa5Og7n6n3g@dpg-cnin16v79t8c73bt7qr0-a.oregon-postgres.render.com/sensitization_app',
   {
-    host: "localhost",
-    dialect: "postgresql",
+    host: 'localhost',
+    dialect: 'postgres', // Use 'postgres' instead of 'postgresql'
   },
 );
-module.exports = sequelize;
+
+export default sequelize;
