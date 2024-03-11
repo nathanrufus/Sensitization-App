168f4f62abb805cb136890bd80a0319983396f96
kibet nathan <nathanrufus540@gmail.com>
2024-03-11 21:16:41 +0300

    adding pg-store

--- src/components/db.js
+++ src/components/db.js
@@ -1,10 +1,14 @@
 import { Sequelize } from 'sequelize';
+import pgHstore from 'pg-hstore';
 
 const sequelize = new Sequelize(
   'postgresql://sensitization_app_user:m43TYykCUSL9t1m9IBdSbYa5Og7n6n3g@dpg-cnin16v79t8c73bt7qr0-a.oregon-postgres.render.com/sensitization_app',
   {
     host: 'localhost',
-    dialect: 'postgres', // Use 'postgres' instead of 'postgresql'
+    dialect: 'postgres',
+    dialectModule: await import('pg'), // Use 'await' if 'import' returns a promise
+    typeValidation: true, // You can add other options if needed
+    logging: false,
   },
 );
 
